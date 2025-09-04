792a6806a6365339845edcaadbf3333dafe42270
Angelo_Nicoli (Gengio) <122599802+AngeloNicoli@users.noreply.github.com>
2025-09-04 19:27:45 +0200

    code clanup

--- Quest_Quest_RPG/Graphic_Drawer.py
+++ Quest_Quest_RPG/Graphic_Drawer.py
@@ -19,10 +19,12 @@ Center_Canvas = [Canvas_width/2,Canvas_height/2]
 canvas = Canvas(width=Canvas_width, height= Canvas_height, bg='gray')  
 canvas.grid(row=0, column=0 , columnspan = 8, padx=5)
 
+Background_color = ["Green", "Red", "Blue", "Red", "Green", "Red", "Green", "Red"]
+
 
 for i in range(0,2):
     for j in range(0,7):
-        Color_01 = Label(root,text="", bg="Green", borderwidth=1,relief="solid",padx=20)
+        Color_01 = Label(root,text="", bg= Background_color[j], borderwidth=1,relief="solid",padx=20)
         Color_01.grid(row= i+1, column=j,pady=2)   
 
 
@@ -36,8 +38,28 @@ Color_01.bind("<Button-1>", lambda e:printatore())
 #Red_Color = Label(root,text="", bg="#c50f1f", borderwidth=1,relief="solid")
 #Red_Color.grid(row=1, column=1,sticky=W+E, padx=5, ipadx=10)
 
+row = 20
+column = 20
+pixel_size = 20
+
+def draw_pixel(row,column,color):
+      #print(row,column,color)
+      cordy = (row) * pixel_size
+      cordx = (column) * pixel_size
+      #print(str(row) + " " + str(column))
+      if color == '0':
+         color = "white"
+      Pages[canvas_page-1].create_rectangle(cordx, cordy, cordx + pixel_size, cordy + pixel_size , width = 0, fill = color , tags="pixel")
+      pixel_color[row][column] = color
 
 
+def mostra_coordinate(event):
+    # event.x e event.y contengono le coordinate relative al Canvas
+    print(f"Click a: x={event.x}, y={event.y}")
+
+
+# Collega il click del mouse (tasto sinistro) alla funzione
+canvas.bind("<Button-1>", mostra_coordinate)
 
 
 
