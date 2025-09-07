bc3f428de226978da7adb1cfaa46ba7af37995c1
Angelo_Nicoli (Gengio) <122599802+AngeloNicoli@users.noreply.github.com>
2025-09-07 21:50:43 +0200

    new asset

--- Quest_Quest_RPG/Graphic_Drawer.py
+++ Quest_Quest_RPG/Graphic_Drawer.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 # Size of Main Window
 window_width = 610
@@ -31,33 +32,39 @@ for i in range(0,2):
 def printatore():
     print("3")
 
-
 Color_01.bind("<Button-1>", lambda e:printatore())
-
    
 #Red_Color = Label(root,text="", bg="#c50f1f", borderwidth=1,relief="solid")
 #Red_Color.grid(row=1, column=1,sticky=W+E, padx=5, ipadx=10)
 
 row = 20
 column = 20
-pixel_size = 20
+pixel_size = 30
 
-def draw_pixel(row,column,color):
+def draw_pixel(row,column,color,pixel_size,cordx,cordy):
       #print(row,column,color)
-      cordy = (row) * pixel_size
-      cordx = (column) * pixel_size
+      #cordy = (row) * pixel_size
+      #cordx = (column) * pixel_size
+      print(cordx)
+      print(cordy)
+      cordx = math.floor(cordx/30) * 30
+      cordy = math.floor(cordy/30) * 30
+      print(cordx)
+      print(cordy)
       #print(str(row) + " " + str(column))
       if color == '0':
-         color = "white"
-      Pages[canvas_page-1].create_rectangle(cordx, cordy, cordx + pixel_size, cordy + pixel_size , width = 0, fill = color , tags="pixel")
-      pixel_color[row][column] = color
+        color = "white"
+      canvas.create_rectangle(cordx, cordy, cordx + pixel_size, cordy + pixel_size , width = 0, fill = color , tags="pixel")
+      #canvas.create_rectangle(0, 0, 30,30, width = 0, fill = color , tags="pixel")
+      #pixel_color[row][column] = color
 
 
 def mostra_coordinate(event):
     # event.x e event.y contengono le coordinate relative al Canvas
+    draw_pixel(30,30,"BLUE",30,event.x,event.y)   #row,column, 
     print(f"Click a: x={event.x}, y={event.y}")
-
-
+    
+    
 # Collega il click del mouse (tasto sinistro) alla funzione
 canvas.bind("<Button-1>", mostra_coordinate)
 
